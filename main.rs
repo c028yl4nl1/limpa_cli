2c0d70ff411db533b599043178f6f66a4dc9ec92
c028yl4nl1 <116520289+c028yl4nl1@users.noreply.github.com>
2023-05-31 10:06:50 -0300

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,5 @@
+
+fn main(){
+print!("\x1B[2J\x1B[1;1H");
+}
+//O primeiro "\x1B2J" representa o comando para limpar a tela. O segundo "\x1B[1;1H" posiciona o cursor na primeira linha e na primeira coluna da tela.
